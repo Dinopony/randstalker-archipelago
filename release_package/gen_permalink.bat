fa8708a5921d4bc3d4f168dc84c92ec362882165
Dinopony <dinopony.gg@gmail.com>
2023-05-01 16:20:28 +0200

    Updated release package contents

--- release_package/gen_permalink.bat
+++
@@ -1,3 +0,0 @@
-cd /D "%~dp0"
-randstalker.exe --outputrom=./seeds/ --permalink --nopause
-pause
--- release_package/gen_preset.bat
+++
@@ -1,3 +0,0 @@
-cd /D "%~dp0"
-randstalker.exe --outputrom=./seeds/ --preset=./presets/default.json --nopause
-pause
