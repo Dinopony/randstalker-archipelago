b064b1f6e3980c9e138891ef3100f0c7d541dd5b
Dinopony <dinopony.gg@gmail.com>
2023-09-29 15:07:31 +0200

    Added error forwarding from randstalker to client

--- src/randstalker_invoker.cpp
+++ src/randstalker_invoker.cpp
@@ -16,17 +16,37 @@
 
 bool invoke(const std::string& command)
 {
+    SECURITY_ATTRIBUTES saAttr;
+    saAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
+    saAttr.bInheritHandle = TRUE;
+    saAttr.lpSecurityDescriptor = nullptr;
+
+    HANDLE pipeReader = nullptr;
+    HANDLE pipeWriter = nullptr;
+    if ( ! CreatePipe(&pipeReader, &pipeWriter, &saAttr, 0) )
+    {
+        Logger::error("Could not create pipe.");
+        return false;
+    }
+    if ( ! SetHandleInformation(pipeReader, HANDLE_FLAG_INHERIT, 0) )
+    {
+        Logger::error("Couldn't make the pipe inherited.");
+        return false;
+    }
+
     STARTUPINFO si;
     ZeroMemory(&si, sizeof(si));
-    si.cb = sizeof(si);
+    si.cb = sizeof(STARTUPINFO);
+    si.hStdError = pipeWriter;
+    si.dwFlags |= STARTF_USESTDHANDLES;
 
     PROCESS_INFORMATION pi;
-    ZeroMemory(&pi, sizeof(pi));
+    ZeroMemory(&pi, sizeof(PROCESS_INFORMATION));
 
     char command_c_str[2048];
     sprintf(command_c_str, "%s", command.c_str());
 
-    if(!CreateProcess(nullptr, command_c_str, nullptr, nullptr, FALSE, PROCESS_FLAGS, nullptr, nullptr, &si, &pi))
+    if(!CreateProcess(nullptr, command_c_str, nullptr, nullptr, TRUE, PROCESS_FLAGS, nullptr, nullptr, &si, &pi))
     {
         Logger::error("Couldn't create Randstalker process to build ROM.");
         return false;
@@ -40,6 +60,17 @@ bool invoke(const std::string& command)
 
     CloseHandle(pi.hProcess);
     CloseHandle(pi.hThread);
+    CloseHandle(pipeWriter);
+
+    // Read output from file
+    DWORD dwRead = 0;
+    CHAR chBuf[4096];
+    while(ReadFile(pipeReader, chBuf, 4096, &dwRead, nullptr) && dwRead != 0)
+    {
+        Logger::error(std::string(chBuf, dwRead));
+    }
+
+    CloseHandle(pipeReader);
     return ec == 0;
 }
 
@@ -92,3 +123,4 @@ std::set<std::string> invoke_randstalker_to_solve_logic(const std::string& comma
 
     return location_names;
 }
+
